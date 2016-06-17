8b8b70b0d0d62e8a5d3370817653c76e25a1e786
Student <student@M05.local>
2016-06-17 20:09:48 +0300

    init proj

---
+++ iVK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iVK
+//
+//  Created by Student on 6/17/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
